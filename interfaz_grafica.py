a7d1910145b7cee523df24b7e352a3b091a4babc
EpsyUwU <estebantareas2003@gmail.com>
2024-05-19 16:57:08 -0600

    ya quedo mi algoritmo genetico

---
+++ interfaz_grafica.py
@@ -0,0 +1,223 @@
+import tkinter as tk
+from tkinter import messagebox
+from main import main
+from PIL import Image, ImageTk, ImageSequence
+from sympy import symbols, lambdify, sympify
+from pandastable import Table, TableModel
+import pandas as pd
+
+def update_table():
+    new_data = pd.read_csv('datos_tabla.csv')
+
+    global data, table
+    data = new_data
+    table.updateModel(TableModel(data))
+    table.redraw()
+def limpiar_frame(frame):
+    for widget in frame.winfo_children():
+        widget.destroy()
+def mostrar_grafica():
+    # Leer la imagen
+    limpiar_frame(gif_frame)
+    im = Image.open('ultima_grafica.png')
+
+    # Crear un PhotoImage a partir de la imagen
+    photo = ImageTk.PhotoImage(im)
+
+    label = tk.Label(gif_frame, image=photo)
+    label.image = photo  # Guardar una referencia a la imagen para evitar que sea eliminada por el recolector de basura
+    label.pack()
+def play_gif():
+    limpiar_frame(gif_frame)
+    # Leer el GIF
+    im = Image.open('output.gif')
+
+    # Crear un PhotoImage para cada frame en el GIF
+    frames = [ImageTk.PhotoImage(frame) for frame in ImageSequence.Iterator(im)]
+
+    label = tk.Label(gif_frame)
+    label.pack()
+
+    def play(index=0):
+        # Mostrar el siguiente frame
+        label.config(image=frames[index])
+        if index < len(frames) - 1:  # Solo repetir si no es el último frame
+            root.after(50, play, index + 1)
+    play()
+
+def ejecutar():
+    try:
+        tamaño_poblacion = int(entry_tamaño_poblacion.get())
+    except ValueError:
+        print("Valor inválido para 'tamaño_poblacion':", entry_tamaño_poblacion.get())
+        messagebox.showerror("Error", "Por favor, ingrese valores válidos")
+        return
+
+    try:
+        radio_mutacion = float(entry_radio_mutacion.get())
+    except ValueError:
+        print("Valor inválido para 'radio_mutacion':", entry_radio_mutacion.get())
+        messagebox.showerror("Error", "Por favor, ingrese valores válidos")
+        return
+
+    try:
+        radio_mutacion_individuo = float(entry_radio_mutacion_individuo.get())
+    except ValueError:
+        print("Valor inválido para 'radio_mutacion_individuo':", entry_radio_mutacion_individuo.get())
+        messagebox.showerror("Error", "Por favor, ingrese valores válidos")
+        return
+
+    try:
+        numero_generaciones = int(entry_numero_generaciones.get())
+    except ValueError:
+        print("Valor inválido para 'numero_generaciones':", entry_numero_generaciones.get())
+        messagebox.showerror("Error", "Por favor, ingrese valores válidos")
+        return
+
+    try:
+        tamaño_maximo_poblacion = int(entry_tamaño_maximo_poblacion.get())
+    except ValueError:
+        print("Valor inválido para 'tamaño_maximo_poblacion':", entry_tamaño_maximo_poblacion.get())
+        messagebox.showerror("Error", "Por favor, ingrese valores válidos")
+        return
+
+    try:
+        limite_inferior = float(entry_limite_inferior.get())
+    except ValueError:
+        print("Valor inválido para 'limite_inferior':", entry_limite_inferior.get())
+        messagebox.showerror("Error", "Por favor, ingrese valores válidos")
+        return
+
+    try:
+        limite_superior = float(entry_limite_superior.get())
+    except ValueError:
+        print("Valor inválido para 'limite_superior':", entry_limite_superior.get())
+        messagebox.showerror("Error", "Por favor, ingrese valores válidos")
+        return
+
+    try:
+        referencia_resolucion = float(entry_referencia_resolucion.get())
+    except ValueError:
+        print("Valor inválido para 'delta_asterisco':", entry_referencia_resolucion.get())
+        messagebox.showerror("Error", "Por favor, ingrese valores válidos")
+        return
+
+    try:
+        valor_n = int(entry_valor_n.get())
+    except ValueError:
+        print("Valor inválido para 'valor_n':", entry_valor_n.get())
+        messagebox.showerror("Error", "Por favor, ingrese valores válidos")
+        return
+
+    try:
+        funcion_str = entry_funcion.get()
+    except ValueError:
+        print("Valor inválido para 'funcion':", entry_funcion.get())
+        messagebox.showerror("Error", "Por favor, ingrese valores válidos")
+        return
+
+    try:
+        tipo_optimizacion = optimizacion.get()
+    except ValueError:
+        print("Valor inválido para 'tipo_optimizacion':", optimizacion.get())
+        messagebox.showerror("Error", "Por favor, ingrese valores válidos")
+        return
+
+    x = symbols('x')
+    funcion_sympy = sympify(funcion_str)
+    funcion = lambdify(x, funcion_sympy, "math")
+
+    main(tamaño_poblacion,tamaño_maximo_poblacion, radio_mutacion, radio_mutacion_individuo, numero_generaciones, limite_inferior, limite_superior, referencia_resolucion, funcion, tipo_optimizacion, valor_n)
+    mostrar_grafica()
+    update_table()
+
+root = tk.Tk()
+
+# Crear un marco principal
+main_frame = tk.Frame(root)
+main_frame.pack()
+
+# Crear un marco para los botones
+button_frame = tk.Frame(main_frame)
+button_frame.pack(side=tk.LEFT)
+
+# Crear un marco para el GIF
+gif_frame = tk.Frame(main_frame)
+gif_frame.pack(side=tk.RIGHT)
+
+label_tamaño_poblacion = tk.Label(button_frame, text="Tamaño de la población")
+label_tamaño_poblacion.pack()
+entry_tamaño_poblacion = tk.Entry(button_frame)
+entry_tamaño_poblacion.pack()
+
+label_tamaño_maximo_poblacion = tk.Label(button_frame, text="Tamaño máximo de la población")
+label_tamaño_maximo_poblacion.pack()
+entry_tamaño_maximo_poblacion = tk.Entry(button_frame)
+entry_tamaño_maximo_poblacion.pack()
+
+label_radio_mutacion = tk.Label(button_frame, text="Radio de mutación")
+label_radio_mutacion.pack()
+entry_radio_mutacion = tk.Entry(button_frame)
+entry_radio_mutacion.pack()
+
+label_radio_mutacion_individuo = tk.Label(button_frame, text="Radio de mutación del individuo")
+label_radio_mutacion_individuo.pack()
+entry_radio_mutacion_individuo = tk.Entry(button_frame)
+entry_radio_mutacion_individuo.pack()
+
+label_numero_generaciones = tk.Label(button_frame, text="Número de generaciones")
+label_numero_generaciones.pack()
+entry_numero_generaciones = tk.Entry(button_frame)
+entry_numero_generaciones.pack()
+
+label_limite_inferior = tk.Label(button_frame, text="Límite inferior")
+label_limite_inferior.pack()
+entry_limite_inferior = tk.Entry(button_frame)
+entry_limite_inferior.pack()
+
+label_limite_superior = tk.Label(button_frame, text="Límite superior")
+label_limite_superior.pack()
+entry_limite_superior = tk.Entry(button_frame)
+entry_limite_superior.pack()
+
+label_referencia_resolucion = tk.Label(button_frame, text="Resolucion de referencia")
+label_referencia_resolucion.pack()
+entry_referencia_resolucion = tk.Entry(button_frame)
+entry_referencia_resolucion.pack()
+
+label_valor_n = tk.Label(button_frame, text="Numero de parejas del individuo")
+label_valor_n.pack()
+entry_valor_n = tk.Entry(button_frame)
+entry_valor_n.pack()
+
+label_funcion = tk.Label(button_frame, text="Ingrese una funcion")
+label_funcion.pack()
+entry_funcion = tk.Entry(button_frame)
+entry_funcion.pack()
+
+label_optimizacion = tk.Label(button_frame, text="Optimización")
+label_optimizacion.pack()
+optimizacion = tk.IntVar(value=1)  # 1 para maximizar, 0 para minimizar
+radio_max = tk.Radiobutton(button_frame, text="Maximizar", variable=optimizacion, value=1)
+radio_max.pack()
+radio_min = tk.Radiobutton(button_frame, text="Minimizar", variable=optimizacion, value=0)
+radio_min.pack()
+
+button = tk.Button(button_frame, text="Ejecutar", command=ejecutar)
+button.pack()
+
+button_gif = tk.Button(button_frame, text="Mostrar video", command=play_gif)
+button_gif.pack()
+
+button_grafica = tk.Button(button_frame, text="Mostrar gráfica", command=mostrar_grafica)
+button_grafica.pack()
+
+table_frame = tk.Frame(main_frame)
+table_frame.pack(side=tk.RIGHT)
+
+data = pd.DataFrame(columns=["Cadena de bits", "Índice", "X", "Aptitud"])
+
+table = Table(table_frame, dataframe=data, showtoolbar=True, showstatusbar=True)
+table.show()
+
+root.mainloop()
---
+++ main.py
@@ -0,0 +1,156 @@
+import itertools
+import random
+import math
+import matplotlib.pyplot as plt
+import imageio
+import pandas as pd
+from PIL import Image
+import numpy as np
+
+
+class Poblacion:
+    def __init__(self, tamaño_poblacion, tamaño_maximo_poblacion, radio_mutacion, radio_mutacion_individuo, numero_generaciones, limite_inferior, limite_superior, referencia_resolucion, funcion, tipo_optimizacion):
+        self.tamaño_poblacion = tamaño_poblacion
+        self.tamaño_maximo_poblacion = tamaño_maximo_poblacion
+        self.radio_mutacion = radio_mutacion
+        self.radio_mutacion_individuo = radio_mutacion_individuo
+        self.numero_generaciones = numero_generaciones
+        self.limite_inferior = limite_inferior
+        self.limite_superior = limite_superior
+        self.referencia_resolucion = referencia_resolucion
+        self.rango = self.limite_superior - self.limite_inferior
+        self.bits = self.numero_bits()
+        self.delta_x = self.deltaX()
+        self.funcion = funcion
+        self.tipo_optimizacion = tipo_optimizacion
+
+    def numero_bits(self):
+        numero_puntos = (self.rango / self.referencia_resolucion) + 1
+        n_bits = math.ceil(math.log2(numero_puntos))
+        return n_bits
+
+    def deltaX(self):
+        deltaX = self.rango / ((2**self.bits - 1))
+        deltaX = round(deltaX, int(math.log10(1/deltaX)) + 1)
+        return deltaX
+
+    def f(self, x):
+        return self.funcion(x)
+
+    def generarPoblacion(self, poblacion):
+        return [self.generarIndividuo() for i in range(poblacion)]
+
+    def generarIndividuo(self):
+        return [int(digit) for digit in bin(random.randint(0, 2**self.bits - 1))[2:].zfill(self.bits)]
+
+    def reproducir(self, poblacion, n):
+        nuevaPoblacion = []
+        for individuo in poblacion:
+            m = random.randint(0, n)  # Genera un número aleatorio m entre [0, n]
+            for _ in range(m):  # Realiza m cruces
+                otroIndividuo = individuo
+                while otroIndividuo == individuo:  # Asegura que el individuo no se cruce consigo mismo
+                    otroIndividuo = random.choice(poblacion)
+                hijo1, hijo2 = self.cruza(individuo, otroIndividuo)
+                nuevaPoblacion.append(hijo1)
+                nuevaPoblacion.append(hijo2)
+        return nuevaPoblacion
+
+    def cruza(self, individuo1, individuo2):
+        puntoCorte = random.randint(0, self.bits - 1)  # Selecciona un punto de cruce aleatorio
+        # Crea el primer y segundo hijo
+        hijo1 = list(itertools.chain(individuo1[:puntoCorte], individuo2[puntoCorte:]))
+        hijo2 = list(itertools.chain(individuo2[:puntoCorte], individuo1[puntoCorte:]))
+        return hijo1, hijo2
+
+    def mutar(self, poblacion):
+        for individuo in poblacion:
+            if random.random() < self.radio_mutacion_individuo:  # Decide si el individuo debe mutar
+                bit = random.randint(0, self.bits - 1)  # Decide qué bit debe mutar
+                individuo[bit] = 1 - individuo[bit]  # Realiza la mutación del gen mediante la negación del bit
+        return poblacion
+
+    def obtenerAptitud(self, individuo):
+        numero = int(''.join(str(bit) for bit in individuo), 2)
+        x = self.limite_inferior + numero * self.delta_x
+        return self.f(x)
+
+    def podar(self, poblacion):
+        # Elimina los individuos duplicados y los convierte de nuevo a listas
+        poblacion = [list(x) for x in set(tuple(x) for x in poblacion)]
+
+        # Calcula la aptitud de cada individuo y los ordena en orden descendente de aptitud
+        poblacion = sorted(poblacion, key=self.obtenerAptitud, reverse=self.tipo_optimizacion)
+
+        # Devuelve los individuos más aptos hasta el tamaño máximo de la población
+        return poblacion[:self.tamaño_maximo_poblacion]
+def main(tamaño_poblacion,tamaño_maximo_poblacion,radio_mutacion_individuo, radio_mutacion, numero_generaciones, limite_inferior, limite_superior, delta_asterisco, funcion, tipo_optimizacion, valor_n):
+    poblacion = Poblacion(tamaño_poblacion,tamaño_maximo_poblacion, radio_mutacion, radio_mutacion_individuo, numero_generaciones, limite_inferior, limite_superior, delta_asterisco, funcion, tipo_optimizacion)
+    poblacionI = poblacion.generarPoblacion(poblacion.tamaño_poblacion)
+
+    mejores = []
+    media = []
+    peores = []
+    imagenes = []
+
+    n = valor_n
+    datos_tabla = pd.DataFrame(columns=['Cadena de bits', 'Índice', 'X', 'Aptitud'])
+
+    for i in range(poblacion.numero_generaciones):
+        hijos = poblacion.reproducir(poblacionI, n)
+        hijosMutados = poblacion.mutar(hijos)
+        poblacionI = tuple(list(poblacionI) + hijosMutados)
+        aptitudes = [poblacion.obtenerAptitud(individuo) for individuo in poblacionI]
+        indice_mejor = aptitudes.index(max(aptitudes))
+        mejor_individuo = poblacionI[indice_mejor]
+        datos_tabla = datos_tabla._append({'Cadena de bits': ''.join(str(bit) for bit in mejor_individuo), 'Índice': i, 'X': poblacion.limite_inferior + int(''.join(str(bit) for bit in mejor_individuo), 2) * poblacion.delta_x, 'Aptitud': poblacion.obtenerAptitud(mejor_individuo)}, ignore_index=True)
+        atributosPoblacion = [poblacion.obtenerAptitud(individuo) for individuo in poblacionI]
+
+        if tipo_optimizacion == 1:
+            mejores.append(max(atributosPoblacion))
+            peores.append(min(atributosPoblacion))
+        elif tipo_optimizacion == 0:
+            mejores.append(min(atributosPoblacion))
+            peores.append(max(atributosPoblacion))
+
+        media.append(sum(atributosPoblacion) / len(atributosPoblacion))
+
+        poblacionI = poblacion.podar(poblacionI)
+
+        # Crear y guardar la gráfica
+        x = range(i + 1)
+        fig, axs = plt.subplots(1, figsize=(5, 5))
+        axs.plot(mejores, label='Mejores')
+        axs.plot(peores, label='Peores')
+        axs.plot(media, label='Media')
+        axs.legend()
+
+        axs.set_title('Evolución de la aptitud de la población')
+        axs.set_xlabel('Generación')
+        axs.set_ylabel('Aptitud')
+        plt.savefig('ultima_grafica.png')
+        print('Mejor individuo:', max(mejores))
+        print('Peor individuo:', min(peores))
+
+        nombre_archivo = f"grafica{i}.png"
+        fig.savefig(nombre_archivo)
+        imagenes.append(imageio.imread(nombre_archivo))
+
+        plt.close(fig)
+
+    datos_tabla.to_csv('datos_tabla.csv', index=False)
+
+    imagenes_resized = [Image.fromarray(img).resize((img.shape[1] // 2, img.shape[0] // 2), Image.LANCZOS) for img in imagenes]
+
+    # Volver a cambiar el tamaño de las imágenes al tamaño original
+    imagenes_resized = [img.resize((img.size[0] * 2, img.size[1] * 2), Image.LANCZOS) for img in imagenes_resized]
+
+    # Convertir las imágenes a arrays de numpy
+    imagenes_resized = [np.array(img) for img in imagenes_resized]
+
+    # Crear un escritor de video
+    writer = imageio.get_writer('output.mp4', fps=0.5)
+
+if __name__ == "__main__":
+    def main():
+        pass
